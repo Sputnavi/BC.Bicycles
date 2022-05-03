fd1ffae29a98abdab887f865261ecf5fad685500
Sputnavi <sputnavik@yandex.by>
2022-05-04 00:09:15 +0300

    Add main API configuration

---
+++ BC.Bicycles/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace BC.Bicycles.Helpers.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services) =>
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "BC.Bicycles",
+                    Description = "Microservice to manage bicycles",
+                });
+            });
+    }
+}
--- BC.Bicycles/Program.cs
+++ BC.Bicycles/Program.cs
@@ -1,6 +1,26 @@
+using BC.Bicycles.Helpers;
+using BC.Bicycles.Helpers.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+services.AddControllers().AddNewtonsoftJson();
+services.ConfigureSwagger();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseMiddleware<ExceptionHandler>();
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
