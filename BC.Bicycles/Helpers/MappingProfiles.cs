b82c69497f7fc34463c92314a833fed36e55a9fd
Sputnavi <sputnavik@yandex.by>
2022-05-06 01:02:20 +0300

    Add mapping profiles

---
+++ BC.Bicycles/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BC.Bicycles.Boundary.Request;
+using BC.Bicycles.Boundary.Response;
+using BC.Bicycles.Models;
+
+namespace BC.Bicycles.Helpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Bicycle, BicycleForReadModel>();
+        CreateMap<BicycleForCreateOrUpdateModel, Bicycle>().ReverseMap();
+        CreateMap<BicycleForReadModel, BicycleForCreateOrUpdateModel>();
+    }
+}
